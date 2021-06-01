583ba2ef3ce998268ee2498100a0b5fbc1b11b7a
gabrielss97 <gabriel.ssalvador97@gmail.com>
2021-06-01 12:26:34 -0300

    80% projeto quiz app

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <h1 align="center" style.color="yellow">10 mini projetos em JavaScrit Vanilla</h1>
 
-###Lista de projetos
+<h3>Lista de projetos<h3>
 
 - [x] Countdown App
 - [] Quiz App
--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,6 +1,6 @@
 quizData = [
   {
-    enunciated: `Acrescente uma "tooltip" ao parágrafo abaixo com o texto "Sobre Quizcode".`,
+    title: `Acrescente uma "tooltip" ao parágrafo abaixo com o texto "Sobre Quizcode".`,
     question: `<p ***="Sobre Quizcode">Quizcode de quiz sobre programação.</p>`,
     a: "h1",
     b: "id",
@@ -9,8 +9,8 @@ quizData = [
     correct: "d",
   },
   {
-    enunciated: `Ajuste o tamanho da imagem para 250 pixels de largura e 400 pixels de altura.`,
-    question: `<img src="w3schools.jpg" width="***" height="***">`,
+    title: `Ajuste o tamanho da imagem para 250 pixels de largura e 400 pixels de altura.`,
+    question: '<img src="w3schools.jpg" width="***" height="***">',
     a: "width=250, height=250",
     b: "width=400, height=250",
     c: "width=250, height=400",
@@ -18,7 +18,7 @@ quizData = [
     correct: "c",
   },
   {
-    enunciated: `Transforme o elemento abaixo em um link que vá para "https://www.quizcode.com.br".`,
+    title: `Transforme o elemento abaixo em um link que vá para "https://www.quizcode.com.br".`,
     question: `<a ***="https://www.quizcode.com.br">Isso é um link.</a>`,
     a: "src",
     b: "href",
@@ -27,7 +27,7 @@ quizData = [
     correct: "b",
   },
   {
-    enunciated: `Especificar um texto alternativo para a imagem. O texto alternativo é útil quando a imagem não pode ser exibida e também para acessibilidade.`,
+    title: `Especificar um texto alternativo para a imagem. O texto alternativo é útil quando a imagem não pode ser exibida e também para acessibilidade.`,
     question: `<img src="quizcode.png" ***="Quizcode Logo">`,
     a: "text",
     b: "alt",
@@ -36,7 +36,7 @@ quizData = [
     correct: "b",
   },
   {
-    enunciated: `Use a tag HTML correta para adicionar um título com o texto "London".`,
+    title: `Use a tag HTML correta para adicionar um título com o texto "London".`,
     question: `<***>London</***>`,
     a: "h1",
     b: "title",
@@ -45,7 +45,7 @@ quizData = [
     correct: "a",
   },
   {
-    enunciated: `Use a tag HTML correta para adicionar um parágrafo com o texto "Hello World!`,
+    title: `Use a tag HTML correta para adicionar um parágrafo com o texto "Hello World!`,
     question: `<***>Hello World!</***>`,
     a: "h1",
     b: "title",
@@ -54,7 +54,7 @@ quizData = [
     correct: "d",
   },
   {
-    enunciated: `Acrescentar uma quebra de linha no meio do parágrafo:`,
+    title: `Acrescentar uma quebra de linha no meio do parágrafo:`,
     question: `<p>João foi ao parque.<***>João encontrou seu amigo lá.</p>`,
     a: "rb",
     b: "ul",
@@ -63,16 +63,16 @@ quizData = [
     correct: "c",
   },
   {
-    enunciated: `Utilize a etiqueta de comentário HTML para fazer um comentário a partir do texto "Isto é um comentário".`,
+    title: `Utilize a etiqueta de comentário HTML para fazer um comentário a partir do texto "Isto é um comentário".`,
     question: `*** Isso é um comentário ***`,
     a: "<!-- -->",
     b: "<-- -->",
     c: "<!- -!>",
     d: "<-- ?-->",
-    correct: "d",
+    correct: "a",
   },
   {
-    enunciated: `Utilize o HTML correto para tornar o texto abaixo em um link para "default.html".`,
+    title: `Utilize o HTML correto para tornar o texto abaixo em um link para "default.html".`,
     question: `<***>Visite nosso tutorial de HTML</***>`,
     a: '<a href="default"> </a>',
     b: '<a href="default.html"> </a>',
@@ -81,7 +81,7 @@ quizData = [
     correct: "b",
   },
   {
-    enunciated: `Utilizar o atributo HTML correto para abrir numa nova janela.`,
+    title: `Utilizar o atributo HTML correto para abrir numa nova janela.`,
     question: `<a href="html_images.asp" ***>HTML Images</a>`,
     a: 'target="_blank"',
     b: 'src="_blank"',
@@ -90,7 +90,7 @@ quizData = [
     correct: "a",
   },
   {
-    enunciated: `Utilizar os atributos de imagem HTML para definir o tamanho da imagem para 250 pixels de largura e 400 pixels de altura.`,
+    title: `Utilizar os atributos de imagem HTML para definir o tamanho da imagem para 250 pixels de largura e 400 pixels de altura.`,
     question: `<img src="scream.png" ***="250" ***="400">`,
     a: "size size",
     b: "height width",
@@ -99,93 +99,122 @@ quizData = [
     correct: "d",
   },
   {
-    enunciated: `Adicionar o atributo HTML correto para exibir a imagem "smiley.gif".`,
-    question: `<img ***>`,
-    a: 'src="smiley.gif"',
-    b: 'src="smiley.png"',
-    c: 'href="smiley.gif"',
-    d: 'href="smiley"',
+    title: `Crie um iframe com um endereço URL que vá para https://www.quizcode.com.br`,
+    question: `<iframe ***="https://www.w3schools.com"></iframe>`,
+    a: "media",
+    b: "href",
+    c: "src",
+    d: "link",
     correct: "c",
   },
   {
-    enunciated: `Acrescentar uma quebra de linha no meio do parágrafo:`,
-    question: `<p>João foi ao parque.<***>João encontrou seu amigo lá.</p>`,
-    a: "rb",
-    b: "ul",
-    c: "br",
-    d: "li",
+    title: `No formulário abaixo, adicionar um campo de entrada com o tipo "botão" e o valor "OK".`,
+    question: `<form> <***> </form>`,
+    a: 'input type="input" value="OK"',
+    b: 'input tag="button" text="OK"',
+    c: 'input type="button" value="OK"',
+    d: 'input tag="button" string="OK"',
     correct: "c",
   },
   {
-    enunciated: `Acrescentar uma quebra de linha no meio do parágrafo:`,
-    question: `<p>João foi ao parque.<***>João encontrou seu amigo lá.</p>`,
-    a: "rb",
-    b: "ul",
-    c: "br",
-    d: "li",
-    correct: "c",
+    title: `Termine o código Form para ele ser funcional`,
+    question: `<form ***="/action_page.js"></form>`,
+    a: "action",
+    b: "play",
+    c: "link",
+    d: "href",
+    correct: "a",
   },
   {
-    enunciated: `Acrescentar uma quebra de linha no meio do parágrafo:`,
-    question: `<p>João foi ao parque.<***>João encontrou seu amigo lá.</p>`,
-    a: "rb",
-    b: "ul",
-    c: "br",
-    d: "li",
-    correct: "c",
+    title: `Especifique que o formulário é submetido utilizando o método "POST".`,
+    question: `<form action="/action_page.js" ***></form>`,
+    a: 'link="post"',
+    b: 'action="post"',
+    c: 'http="post"',
+    d: 'method="post"',
+    correct: "d",
   },
   {
-    enunciated: `Acrescentar uma quebra de linha no meio do parágrafo:`,
-    question: `<p>João foi ao parque.<***>João encontrou seu amigo lá.</p>`,
-    a: "rb",
-    b: "ul",
-    c: "br",
-    d: "li",
-    correct: "c",
+    title: `Desative o input abaixo.`,
+    question: `<input type="text" ***>`,
+    a: "disable",
+    b: "off",
+    c: "enable",
+    d: "dontUSe",
+    correct: "a",
   },
   {
-    enunciated: `Acrescentar uma quebra de linha no meio do parágrafo:`,
-    question: `<p>João foi ao parque.<***>João encontrou seu amigo lá.</p>`,
-    a: "rb",
-    b: "ul",
-    c: "br",
-    d: "li",
-    correct: "c",
-  },
-  {
-    enunciated: `Acrescentar uma quebra de linha no meio do parágrafo:`,
-    question: `<p>João foi ao parque.<***>João encontrou seu amigo lá.</p>`,
-    a: "rb",
-    b: "ul",
-    c: "br",
-    d: "li",
-    correct: "c",
-  },
-  {
-    enunciated: `Acrescentar uma quebra de linha no meio do parágrafo:`,
-    question: `<p>João foi ao parque.<***>João encontrou seu amigo lá.</p>`,
-    a: "rb",
-    b: "ul",
-    c: "br",
-    d: "li",
-    correct: "c",
-  },
-  {
-    enunciated: `Acrescentar uma quebra de linha no meio do parágrafo:`,
-    question: `<p>João foi ao parque.<***>João encontrou seu amigo lá.</p>`,
-    a: "rb",
-    b: "ul",
-    c: "br",
-    d: "li",
-    correct: "c",
-  },
-  {
-    enunciated: `Acrescentar uma quebra de linha no meio do parágrafo:`,
-    question: `<p>João foi ao parque.<***>João encontrou seu amigo lá.</p>`,
-    a: "rb",
-    b: "ul",
-    c: "br",
-    d: "li",
-    correct: "c",
+    title: `Defina o número máximo de caracteres permitidos no campo de entrada para 40.`,
+    question: `<input type="text" ***>`,
+    a: 'minlength="40"',
+    b: 'maxlength="40"',
+    c: 'maxsize="40"',
+    d: 'minsize="40"',
+    correct: "b",
   },
 ];
+
+let btn = document.querySelector("#btn");
+let title = document.querySelector("#title");
+let question = document.querySelector("#question");
+let a = document.querySelector("#questionA");
+let b = document.querySelector("#questionB");
+let c = document.querySelector("#questionC");
+let d = document.querySelector("#questionD");
+const answerEls = document.querySelectorAll(".answer");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswer();
+
+  const currentQuizData = quizData[currentQuiz];
+
+  title.innerText = currentQuizData.title;
+  question.innerText = currentQuizData.question;
+  a.innerText = currentQuizData.a;
+  b.innerText = currentQuizData.b;
+  c.innerText = currentQuizData.c;
+  d.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+  let answer = undefined;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+
+function deselectAnswer() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
+
+btn.addEventListener("click", () => {
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer == quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      //TODO: Show results
+      alert(
+        "Parabéns programador, agora você está um passo mais perto do sucesso"
+      );
+    }
+  }
+});
